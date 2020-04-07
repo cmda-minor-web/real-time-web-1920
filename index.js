c856234da0c637d2831014b93f958a1b8067f529
deannabosschert <deanna.bosschert@gmail.com>
2020-04-07 18:21:32 +0200

    change port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/src/index.html');
@@ -11,5 +13,5 @@ io.on('connection', function(socket) {
 });
 
 http.listen(3000, function() {
-  console.log('listening on port 3000');
+  console.log(`Example app listening on port ${port}!`)
 });
