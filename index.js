b8070b43c164a782cb22c310dc3455031c97d300
deannabosschert <deanna.bosschert@gmail.com>
2020-04-07 17:10:17 +0200

    serve html from file

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
+
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
