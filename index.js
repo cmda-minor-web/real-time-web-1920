d0a542da919213e8b67a279844eace8aaa6fd1be
deannabosschert <deanna.bosschert@gmail.com>
2020-04-07 18:24:18 +0200

    refactor: remove semicolons

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/src/index.html');
-});
+  res.sendFile(__dirname + '/src/index.html')
+})
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
-});
+  console.log('a user connected')
+})
 
 http.listen(3000, function() {
   console.log(`Example app listening on port ${port}!`)
-});
+})
