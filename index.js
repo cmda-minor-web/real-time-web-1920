71e6667db031a71a4db42edd852bf76afff451f4
deannabosschert <deanna.bosschert@gmail.com>
2020-04-07 18:23:21 +0200

    change var to const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 
