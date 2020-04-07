e0556b738fb4f88c60d929665a0cd8bc17334774
deannabosschert <deanna.bosschert@gmail.com>
2020-04-07 18:25:20 +0200

    refactor: remove merge markers

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
-=======
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
->>>>>>> 71e6667db031a71a4db42edd852bf76afff451f4
 
 
 app.get('/', function(req, res) {
