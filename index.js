68c1df0e33416415a0b7b136395c8e658890d27f
deannabosschert <deanna.bosschert@gmail.com>
2020-04-10 10:09:10 +0200

    divide in seperate functions && serve express

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const app = require('express')()
-const http = require('http').createServer(app)
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const path = require('path')
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, 'src')))
+  .get('/', chatbox)
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/src/index.html')
-})
-
-io.on('connection', function(socket) {
-  console.log('a user connected')
-})
-
+io.on('connection', users)
+io.on('connection', messages)
 http.listen(port, function() {
   console.log(`App listening on port ${port}!`)
 })
+
+function chatbox(req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+}
+
+function users(socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function() {
+    console.log('user disconnected')
+  })
+}
+
+function messages(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg)
+  })
+}
+
+function chatman() {
+  io.emit('message', 'Hello')
+}
+
+
+// bot:
+// if [certainWord] in message {
+//   respond with "whatever sentence"
+// }
