a86232eaa2f68462993a0b62d1d6bc6612699cea
deannabosschert <deanna.bosschert@gmail.com>
2020-04-14 15:23:57 +0200

    refactor: replace double quotes by single quotes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").Server(app)
-const io = require("socket.io")(http)
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-  res.render("index", {});
+app.get('/', function(req, res) {
+  res.render('index', {});
 });
 
 
-io.on("connection", function(socket) {
+io.on('connection', function(socket) {
 
-  socket.on("user_join", function(data) {
+  socket.on('user_join', function(data) {
     this.username = data
-    socket.broadcast.emit("user_join", data)
+    socket.broadcast.emit('user_join', data)
   })
 
-  socket.on("chat_message", function(data) {
+  socket.on('chat_message', function(data) {
     data.username = this.username
-    socket.broadcast.emit("chat_message", data)
+    socket.broadcast.emit('chat_message', data)
   })
 
-  socket.on("disconnect", function(data) {
-    socket.broadcast.emit("user_leave", this.username)
+  socket.on('disconnect', function(data) {
+    socket.broadcast.emit('user_leave', this.username)
   })
 })
 
 http.listen(port, function() {
-  console.log("Listening on *:" + port)
+  console.log('Listening on *:' + port)
 })
