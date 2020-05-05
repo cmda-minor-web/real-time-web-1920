9d73ce7b4718ee2f35af98cadac49872cb01bf6f
deannabosschert <deanna.bosschert@gmail.com>
2020-05-05 11:02:00 +0200

    continuous checking for new data now works

--- index.js
+++ index.js
@@ -43,35 +43,92 @@ app.get('/', function(req, res) {
 })
 
 const options2 = {
-  screen_name: 'jeffreestar',
-  count: 5
+  screen_name: 'alldayoptimism',
+  count: 1
 }
 
 io.on('connection', socket => {
 
+  socket.on("start", data => {
+    console.log('twee')
+    console.log(data)
+    getTweet()
+  })
+
   socket.on('new_tweet', data => {
+    console.log('zes')
     socket.broadcast.emit('new_tweet', data)
+    // getTweet()
+  })
+
+  socket.on('refresh_tweet', data => {
+    console.log(data)
+    console.log("elf")
+    // io.emit("new_tweet", tweet)
+    getTweet()
   })
 })
 
 
 
-client.get('statuses/user_timeline', options2, function(err, data) {
-  const tweets = data.map(function(item) {
-    const tweet = item.text
+function getTweet() {
+  console.log('drie')
+  const getData = new Promise(resolve => {
+    client.get('statuses/user_timeline', options2, function(err, data) {
+      const tweets = data.map(function(item) {
+        // console.log(item.user.followers_count)
+        const tweet = item.text
+        return tweet
+      })
+      const tweet = tweets[0]
+      console.log('vier')
+      resolve(tweet)
+    })
+  })
+
+  getData.then(tweet => {
+      console.log('vijf')
+      io.emit("new_tweet", tweet)
+    })
+    .then(() => {
+      console.log('negen')
+      refreshTweet()
+    })
+}
+
+function refreshTweet() {
+  console.log('tien')
+  const data = 'yeet'
+  io.emit("refresh_tweet", data)
+
+}
+
+
+
+
+
+async function getTwet() {
+  const help = await client.get('statuses/user_timeline', options2, function(err, data) {
+    const tweets = data.map(function(item) {
+      const tweet = item.text
+      return tweet
+    })
+    const tweet = tweets[0]
+    // const tweet2 = tweets[1]
+    // const tweet3 = tweets[2]
+    // console.log("test1" + tweet)
+    console.log(tweet)
     return tweet
+    // io.emit("new_tweet", tweet2)
+    // io.emit("new_tweet", tweet3)
+
+    // io.emit("new_tweet", tweet1)
   })
-  const tweet1 = tweets[0]
-  const tweet2 = tweets[1]
-  const tweet3 = tweets[2]
+  // console.log(help)
+  // const tweet = help..
+  // io.emit("new_tweet", tweet)
 
-  io.emit("new_tweet", tweet1)
-  io.emit("new_tweet", tweet2)
-  io.emit("new_tweet", tweet3)
-
-  // io.emit("new_tweet", tweet1)
-
-})
+}
 //
 // var options2 = {
 //   track: 'javascript'
--- public/main.js
+++ public/main.js
@@ -1,9 +1,15 @@
 const tweets = document.querySelector(".tweets")
 const socket = io()
 
-socket.on("new_tweet", function(data) {
-  addTweet(data)
+socket.on("new_tweet", function(tweet) {
+  console.log('zeven')
+  addTweet(tweet)
+  socket.emit("refresh_tweet", 'yeet')
 })
+
+
+socket.emit("start", 'hallo')
+
 
 function addTweet(tweet) {
   const li = document.createElement("li")
