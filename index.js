12e7b4177cd00361c6907f100820cca1134e1b8a
deannabosschert <deanna.bosschert@gmail.com>
2020-05-05 12:04:49 +0200

    refactor: delete unused lines && console.log && comments

--- index.js
+++ index.js
@@ -14,53 +14,39 @@ require('dotenv').config()
 const port = process.env.PORT
 const url = process.env.MNG_URL
 const dbName = process.env.DB_NAME
-
 const client = new Twitter({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET_KEY,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
-
-
-// const apiKey = process.env.TWITTER_API_KEY
-// const apiSecretKey = process.env.TWITTER_API_SECRET_KEY
-// const apiAccessToken = process.env.TWITTER_ACCESS_TOKEN
-// const apiSecretAccessToken = process.env.TWITTER_ACCESS_TOKEN_SECRET
-
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
-app.get('/', function(req, res) {
-  // Api.get().then(data => {
-  res.render('index', {})
-  // })
-})
-
-const options2 = {
-  screen_name: 'alldayoptimism',
+const filterOptions = {
+  screen_name: 'jeffreestar',
   count: 1
 }
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.get('/', function(req, res) {
+  res.render('index', {})
+})
+
+
 io.on('connection', socket => {
 
-  socket.on("start", data => {
+  socket.on("start", () => {
     getTweet()
   })
 
   socket.on('new_tweet', data => {
     socket.broadcast.emit('new_tweet', data)
-    // getTweet()
   })
 
-  socket.on('refresh_tweet', tweet => {
-    const latest_tweet = tweet
-    // io.emit("new_tweet", tweet)
+  socket.on('refresh_tweet', latest_tweet => {
     getTweet(latest_tweet)
   })
 })
@@ -68,7 +54,7 @@ io.on('connection', socket => {
 
 function getTweet(latest_tweet) {
   const getData = new Promise((resolve) => {
-    client.get('statuses/user_timeline', options2, function(err, data) {
+    client.get('statuses/user_timeline', filterOptions, function(err, data) {
       const tweets = data.map(function(item) {
         // console.log(item.user.followers_count)
         const tweet = item.text
@@ -85,15 +71,10 @@ function getTweet(latest_tweet) {
         console.log('same old')
         refreshTweet(tweet)
       } else {
-        console.log('sunshine and more: new tweet!')
+        console.log('sunshine and rainbows: new tweet!')
         io.emit("new_tweet", tweet)
       }
     })
-    // .then(() => {
-    //   console.log('negen')
-    //   refreshTweet()
-    // })
-
     .catch(err => {
       console.log(err)
     })
@@ -101,94 +82,7 @@ function getTweet(latest_tweet) {
 
 function refreshTweet(latest_tweet) {
   getTweet(latest_tweet)
-  // io.emit("refresh_tweet")
 }
-
-function yeet() {
-  console.log('esketit')
-}
-
-
-
-
-
-async function getTwet() {
-  const help = await client.get('statuses/user_timeline', options2, function(err, data) {
-    const tweets = data.map(function(item) {
-      const tweet = item.text
-      return tweet
-    })
-    const tweet = tweets[0]
-    // const tweet2 = tweets[1]
-    // const tweet3 = tweets[2]
-    // console.log("test1" + tweet)
-    console.log(tweet)
-    return tweet
-    // io.emit("new_tweet", tweet2)
-    // io.emit("new_tweet", tweet3)
-
-    // io.emit("new_tweet", tweet1)
-  })
-  // console.log(help)
-  // const tweet = help..
-  // io.emit("new_tweet", tweet)
-
-}
-//
-// var options2 = {
-//   track: 'javascript'
-//   // count: 3
-// };
-
-//
-// client.stream('statuses/filter', options2, function(stream) {
-//   stream.on('data', function(data) {
-//     console.log(data)
-//
-//     // console.log(tweet.text)
-//     // const data = tweet.text
-//     // io.emit("new_tweet", data)
-//   })
-//
-//   stream.on('error', function(error) {
-//     console.log(error)
-//   })
-// })
-
-//
-// client.stream('statuses/user_timeline', {
-//   screen_name: 'jeffreestar',
-//   count: 5
-// }, function(stream) {
-//   stream.on('data', function(tweet) {
-//
-//     console.log(tweet)
-//     console.log(tweet.text)
-//     const data = tweet.text
-//     io.emit("new_tweet", data)
-//   })
-//
-//   stream.on('error', function(error) {
-//     console.log(error)
-//   })
-// })
-
-// const endpoint = "https://api.twitter.com/1.1/users/show.json?screen_name=jeffreestar"
-
-
-// const Api = {
-//   get() {
-//     return fetch(endpoint)
-//       .then(res => res.json())
-//       .then((json) => {
-//         console.log(json)
-//         return json
-//       })
-//       .catch(err => {
-//         console.log(err)
-//       })
-//   }
-// }
 
 http.listen(port, () => {
   console.log('App listening on: ' + port)
--- public/main.js
+++ public/main.js
@@ -2,13 +2,12 @@ const tweets = document.querySelector(".tweets")
 const socket = io()
 
 socket.on("new_tweet", function(tweet) {
-  console.log('zeven')
   addTweet(tweet)
   socket.emit("refresh_tweet", tweet)
 })
 
 
-socket.emit("start", 'hallo')
+socket.emit("start")
 
 
 function addTweet(tweet) {
